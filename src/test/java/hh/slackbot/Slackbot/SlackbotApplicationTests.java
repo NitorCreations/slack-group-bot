e28c8feafa3bad8a7c992613e4a0ac8819b58054
Jurzo <juuso.kalliomaki96@gmail.com>
2022-01-21 10:42:08 +0200

    initial commit

---
+++ src/test/java/hh/slackbot/Slackbot/SlackbotApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.slackbot.Slackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
