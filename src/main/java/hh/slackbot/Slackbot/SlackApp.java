eb7eae902e32f1c63ad3c72cff3062c1dd0955fb
Jurzo <juuso.kalliomaki96@gmail.com>
2022-01-21 19:18:59 +0200

    bot mention respond

--- src/main/java/hh/slackbot/Slackbot/SlackApp.java
+++ src/main/java/hh/slackbot/Slackbot/SlackApp.java
@@ -1,6 +1,7 @@
 package hh.slackbot.Slackbot;
 
 import com.slack.api.bolt.App;
+import com.slack.api.model.event.AppMentionEvent;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,13 @@ public class SlackApp {
         App app = new App();
         app.command("/hello", (req, ctx) -> {
             return ctx.ack(":wave: World");
+        });
+
+        app.event(AppMentionEvent.class, (req, ctx) -> {
+            ctx.say("Greetings :wave:");
+            System.out.println(ctx.getRequestUserId());
+            System.out.println(ctx.getRequestUserToken());
+            return ctx.ack();
         });
 
         return app;
