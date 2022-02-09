63611005c6f50b81126bc3c0c5747c85a67e398f
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2022-02-09 11:39:19 +0200

    Add dummy test class

---
+++ src/test/java/hh/slackbot/Slackbot/SlackAppTest.java
@@ -0,0 +1,24 @@
+package hh.slackbot.Slackbot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackAppTest {
+
+	@Test
+	@DisplayName("Return true if value equals one")
+	void returnTrueIfValueEqualsOne() {
+		SlackApp slackApp = new SlackApp();
+		Assertions.assertTrue(slackApp.getSampleString(1));
+	}
+
+	@Test
+	@DisplayName("Return false if falue is not one")
+	void returnFalseIfValueIsNotOne() {
+		SlackApp slackApp = new SlackApp();
+		Assertions.assertFalse(slackApp.getSampleString(0));
+	}
+}
