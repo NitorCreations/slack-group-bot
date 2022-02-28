dacaf91f2aca6aa33328f3478974c98c24620855
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2022-02-28 19:40:38 +0200

    checkstyle cli linting

--- src/main/java/hh/slackbot/Slackbot/SlackbotApplication.java
+++ src/main/java/hh/slackbot/Slackbot/SlackbotApplication.java
@@ -3,11 +3,13 @@ package hh.slackbot.Slackbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is a comment.
+ */
 @SpringBootApplication
 public class SlackbotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SlackbotApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SlackbotApplication.class, args);
+  }
 }
