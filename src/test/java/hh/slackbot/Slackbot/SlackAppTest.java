640d3d0ca92081f901037acace3fcb7ac2145581
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2022-03-14 18:13:35 +0200

    checkstyle check test dir too

--- src/test/java/hh/slackbot/Slackbot/SlackAppTest.java
+++ src/test/java/hh/slackbot/Slackbot/SlackAppTest.java
@@ -3,8 +3,11 @@ package hh.slackbot.slackbot;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
 
+import com.slack.api.app_backend.events.payload.EventsApiPayload;
+import com.slack.api.bolt.context.builtin.EventContext;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.model.event.AppMentionEvent;
 import java.io.IOException;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,45 +17,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.slack.api.app_backend.events.payload.EventsApiPayload;
-import com.slack.api.bolt.context.builtin.EventContext;
-import com.slack.api.methods.SlackApiException;
-import com.slack.api.model.event.AppMentionEvent;
-
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SlackAppTest {
 
   @Autowired
   SlackApp slackApp;
-	
+
   @MockBean
   private EventsApiPayload<AppMentionEvent> mockReq;
-  
+
   @MockBean
   private EventContext mockCtx;
-  
+
   @BeforeEach
   public void init() {
-	  MockitoAnnotations.openMocks(this);
+    MockitoAnnotations.openMocks(this);
   }
-  
 
   @Test
   @DisplayName("SlackApp is not null")
   void returnAppSuccessfully() {
-	  assertNotNull(slackApp.initSlackApp());  
+    assertNotNull(slackApp.initSlackApp());
   }
-  
-  
+
   @Test
   @DisplayName("SlackApp mention response is successful")
   void mentionResponseSuccessfully() throws IOException, SlackApiException {
+    // mockReq and mockCtx are defined with @MockBean
+    SlackApp.mentionResponse(mockReq, mockCtx);
 
-	  //mockReq and mockCtx are defined with @MockBean
-	  SlackApp.mentionResponse(mockReq, mockCtx);
-	 
-	  verify(mockCtx).say("Greetings :wave:");
-	  verify(mockCtx).ack();  
+    verify(mockCtx).say("Greetings :wave:");
+    verify(mockCtx).ack();
   }
 }
