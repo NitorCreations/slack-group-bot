9ce7c5fa3efe6edd89292bf9d02aebfd38e1a45e
Juuso Kalliomäki <juuso.kalliomaki96@gmail.com>
2022-04-24 12:44:45 +0300

    create button placement

--- src/main/java/hh/slackbot/Slackbot/util/BlockMessager.java
+++ src/main/java/hh/slackbot/Slackbot/util/BlockMessager.java
@@ -10,6 +10,7 @@ import static com.slack.api.model.block.element.BlockElements.button;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.block.element.BlockElement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -25,12 +26,8 @@ public class BlockMessager {
 
   private static final Logger logger = LoggerFactory.getLogger(MessageUtil.class);
 
-  public boolean similarGroupsMessage(
-      String actual,
-      List<String> similar,
-      String channelId,
-      String userId
-  ) {
+  public boolean similarGroupsMessage(String actual, List<String> similar, String channelId,
+      String userId) {
     List<LayoutBlock> blocks = similarGroupsLayout(actual, similar);
     return msgUtil.sendEphemeralResponse(blocks, "Groups with similar names", userId, channelId);
   }
@@ -40,36 +37,27 @@ public class BlockMessager {
     List<BlockElement> blocks = stringsToButtons(similar);
 
     layout.add(
-        section(section ->
-          section
-            .text(markdownText(String.format("Given group was *%s*", actual)))
-            .accessory(
-              button(b -> 
-                b.text(plainText(pt -> pt.text("create and join")))
-                    .value(actual).actionId("btn_create")
-              )
-            ).blockId("header"))
-    );
+        section(section -> section.text(markdownText(String.format("Given group was *%s*", actual)))
+            .blockId("header")));
+
+    layout.add(actions(actions -> actions.elements(Arrays.asList(button(b -> b
+        .text(plainText(pt -> pt.text("create and join"))).value(actual).actionId("btn_create"))))
+        .blockId("creation")));
 
     layout.add(divider());
 
-    layout.add(
-        section(section -> section.text(
-          plainText(pt -> pt.text("Did you mean one of these? click to join."))
-        ).blockId("similar"))
-    );
-    layout.add(actions(actions -> actions.elements(blocks).blockId("asdf")));
+    layout.add(section(section -> section
+        .text(plainText(pt -> pt.text("Did you mean one of these? click to join.")))
+        .blockId("similar")));
+    layout.add(actions(actions -> actions.elements(blocks).blockId("joining")));
 
     return layout;
   }
 
   private List<BlockElement> stringsToButtons(List<String> strings) {
-    return strings
-        .stream()
-        .map(string -> 
-          button(b ->
-            b.text(plainText(pt -> pt.text(string))).value(string).actionId("join_" + string)
-        ))
+    return strings.stream()
+        .map(string -> button(
+            b -> b.text(plainText(pt -> pt.text(string))).value(string).actionId("join_" + string)))
         .collect(Collectors.toList());
   }
 }
